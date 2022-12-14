53af2f9e3c4bd8e07845a978a73bc122e7a70e96
Aluerie <aluerie@gmail.com>
2022-12-14 20:03:41 +0300

    Forgot config

--- TODO.md
+++ TODO.md
@@ -1,3 +1,3 @@
 ### TODO
 
-Currently I'm trying out [GitHub Project](https://github.com/users/Aluerie/projects/7/views/1)
+Currently, I'm trying out [GitHub Project](https://github.com/users/Aluerie/projects/7/views/1)
---
+++ cogs/utils/config.py
@@ -0,0 +1,116 @@
+"""
+Inspired by RoboDanny's `config.py`. So all credit to Danny.
+Very educational though.
+"""
+from __future__ import annotations
+
+import json
+import os
+from typing import TYPE_CHECKING, Any, Dict, Generic, TypeVar, Union, Optional, Type
+import uuid
+import asyncio
+
+if TYPE_CHECKING:
+    from asyncpg import Pool
+
+_T = TypeVar('_T')
+
+
+class Config(Generic[_T]):
+    """The 'Database' objects based on `.json` files :D"""
+
+    def __init__(
+        self,
+        filename: str,
+        pool: Pool = None,
+        *,
+        encoder: Optional[Type[json.JSONEncoder]] = None,
+    ):
+        self.filename = filename
+        self.pool = pool
+        self.encoder = encoder
+        self.loop = asyncio.get_running_loop()
+        self.lock = asyncio.Lock()
+        self._json: Dict[int, Union[Any]] = {}
+        if self.load_from_file():
+            pass
+        elif pool:
+            self.loop.create_task(self.load_from_database())
+            self.loop.create_task(self.save())
+
+    def load_from_file(self) -> bool:
+        try:
+            with open(self.filename, 'r', encoding='utf-8') as f:
+                self._json = json.load(f)
+                return True
+        except FileNotFoundError:
+            return False
+
+    async def load_from_database(self):
+        ...
+
+    def _dump(self):
+        temp = f'{uuid.uuid4()}-{self.filename}.tmp'
+        with open(temp, 'w', encoding='utf-8') as tmp:
+            json.dump(self._json.copy(), tmp, ensure_ascii=True, cls=self.encoder, separators=(',', ':'))
+        # atomically move the file
+        os.replace(temp, self.filename)
+
+    async def save(self) -> None:
+        async with self.lock:
+            await self.loop.run_in_executor(None, self._dump)
+
+    def get(self, key: Any, default: Any = None):
+        """Retrieves a config entry"""
+        return self._json.get(key, default)
+
+    async def put_into_database(self, key: Any, value: Union[_T, Any]):
+        ...
+
+    async def put(self, key: Any, value: Union[_T, Any]) -> None:
+        """Edits a config entry"""
+        self._json[key] = value
+        await self.put_into_database(key, value)
+        await self.save()
+
+    async def remove_from_database(self, key: Any):
+        ...
+
+    async def remove(self, key: Any) -> None:
+        """Removes a config entry."""
+        del self._json[key]
+        await self.remove_from_database(key)
+        await self.save()
+
+    def __contains__(self, item: Any) -> bool:
+        return str(item) in self._json
+
+    def __getitem__(self, item: Any) -> Union[_T, Any]:
+        return self._json[item]
+
+    def __len__(self) -> int:
+        return len(self._json)
+
+    def all(self) -> Dict[Any, Union[_T, Any]]:
+        return self._json
+
+
+class PrefixConfig(Config):
+    def __init__(self, pool: Pool):
+        super().__init__(
+            filename='prefixes.json',
+            pool=pool
+        )
+
+    async def load_from_database(self):
+        query = 'SELECT id, prefix FROM guilds'
+        rows = await self.pool.fetch(query) or []
+        self._json = {r.id: r.prefix for r in rows if r.prefix is not None}
+
+    async def put_into_database(self, guild_id: int, new_prefix: str):
+        query = 'UPDATE guilds SET prefix=$1 WHERE id=$2'
+        await self.pool.execute(query, new_prefix, guild_id)
+
+    async def remove_from_database(self, guild_id: int):
+        query = 'UPDATE guilds SET prefix=NULL WHERE id=$1'
+        await self.pool.execute(query, guild_id)

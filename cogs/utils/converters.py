46ae9760e0298872967daf41c2c448d7adc7dffb
Aluerie <aluerie@gmail.com>
2023-01-21 13:29:23 +0300

    Self was not in typing for 3.10

--- cogs/utils/converters.py
+++ cogs/utils/converters.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING, Optional, Self
+from typing import TYPE_CHECKING, Optional
+from typing_extensions import Self
 
 from discord.ext import commands
 

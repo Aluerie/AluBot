33ce678c66715e5d1eb8a310ee624ce7e41ed1d8
Aluerie <aluerie@outlook.com>
2023-03-02 19:16:27 +0300

    fpc.dota got forgotten

--- cogs/fpc/__init__.py
+++ cogs/fpc/__init__.py
@@ -1,5 +1,10 @@
 """
 FPC is Favourite Player+Character
 """
-from .dota import *
-from .lol import *
+from .dota import setup as dota_setup
+from .lol import setup as lol_setup
+
+
+async def setup(bot):
+    await dota_setup(bot)
+    await lol_setup(bot)

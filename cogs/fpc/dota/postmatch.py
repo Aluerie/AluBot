3980b937987b85be662c96d6d7fd0be96d49394f
Aluerie <aluerie@outlook.com>
2023-05-21 00:40:04 +0300

    Update postmatch.py

--- cogs/fpc/dota/postmatch.py
+++ cogs/fpc/dota/postmatch.py
@@ -50,7 +50,7 @@ class DotaPostMatchEdit(AluCog):
                 query = "SELECT * FROM dota_messages WHERE match_id=$1"
                 for r in await self.bot.pool.fetch(query, row.match_id):
                     for player in pl_dict_list:
-                        if player["character_id"] == r.character_id:
+                        if player["hero_id"] == r.character_id:
                             self.postmatch_players.append(
                                 PostMatchPlayerData(
                                     player_data=player,

d42f1378b4af43f8eb300e6594d204f0dacf38e0
Aluerie <aluerie@outlook.com>
2023-06-28 01:33:06 +0300

    back

--- exts/dota_news/steamdb.py
+++ exts/dota_news/steamdb.py
@@ -13,20 +13,17 @@ But for now I just repost messages I'm interested it to only my channel.
 
 from __future__ import annotations
 
-import asyncio
 from typing import TYPE_CHECKING
 
 import discord
 from discord.ext import commands, tasks
 from github import Github
 
-from config import GIT_PERSONAL_TOKEN
+from config import DOTA_NEWS_WEBHOOK, GIT_PERSONAL_TOKEN, PINK_TEST_WEBHOOK
 from utils import AluCog
 from utils.const import Channel
-from utils.formats import block_function
 from utils.github import human_commit
 from utils.imgtools import str_to_file
-from utils.links import move_link_to_title, replace_tco_links
 
 if TYPE_CHECKING:
     from utils import AluBot
@@ -85,21 +82,29 @@ class SteamDB(AluCog):
     def news_channel(self) -> discord.TextChannel:
         return self.community.dota_news
 
+    @discord.utils.cached_property
+    def news_webhook(self) -> discord.Webhook:
+        return discord.Webhook.from_url(
+            url=PINK_TEST_WEBHOOK if self.bot.test else DOTA_NEWS_WEBHOOK,
+            client=self.bot,
+            session=self.bot.session,
+            bot_token=self.bot.http.token,
+        )
+
     @commands.Cog.listener()
-    async def on_message(self, msg: discord.Message):
+    async def on_message(self, message: discord.Message):
         try:
-            match msg.channel.id:
+            match message.channel.id:
                 case Channel.copy_dota_info:
-                    if "https://steamdb.info" in msg.content:
+                    if "https://steamdb.info" in message.content:
                         url, embeds, files = await get_gitdiff_embed()
-                        msg = await self.news_channel.send(content=f"<{url}>", embeds=embeds)
-                        await msg.publish()
+                        message = await self.news_channel.send(content=f"<{url}>", embeds=embeds)
+                        await message.publish()
                         if len(files):
-                            msg = await self.news_channel.send(files=files)
-                            await msg.publish()
-                    if "https://steamcommunity.com" in msg.content:
-                        files = [await x.to_file() for x in msg.attachments]
-                        msg = await self.news_channel.send(content=msg.content, embeds=msg.embeds, files=files)
+                            message = await self.news_channel.send(files=files)
+                            await message.publish()
+                    if "https://steamcommunity.com" in message.content:
+                        msg = await self.news_webhook.send(content=message.content, wait=True)
                         await msg.publish()
                 # case Channel.copy_dota_steam: # kinda no point, actually.
                 #     if block_function(msg.content, self.blocked_words, self.whitelist_words):
@@ -115,10 +120,12 @@ class SteamDB(AluCog):
                 #     if embeds:
                 #         msg = await self.news_channel.send(embeds=embeds)
                 #         await msg.publish()
-                # case Channel.copy_steam_beta: # no hype anymore
-                #     if "SteamClientBeta" in msg.content:
-                #         files = [await x.to_file() for x in msg.attachments]
-                #         msg = await self.hideout.repost.send(content=msg.content, embeds=msg.embeds, files=files)
+                case Channel.copy_steam_beta:  # no hype anymore
+                    if "SteamClientBeta" in message.content:
+                        files = [await x.to_file() for x in message.attachments]
+                        msg = await self.hideout.repost.send(
+                            content=message.content, embeds=message.embeds, files=files
+                        )
         except Exception as error:
             await self.bot.send_traceback(error, where="#dota-dota_news copypaste")
 

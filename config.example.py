502c050ed090a3fe7e775f4f875173b31603e7aa
Aluerie❤ <aluerie@outlook.com>
2023-11-19 21:59:50 +0300

    😵 Update config example

--- config.example.py
+++ config.example.py
@@ -1,39 +1,43 @@
 # /* cSpell:disable */
 
-# DISCORD
-MAIN_TOKEN = ''  # your bot's token
+# DISCORD (AluBot)
+MAIN_TOKEN = ""  # your bot's token
 
-TEST_TOKEN = ''
+# DISCORD (YenBot)
+TEST_TOKEN = ""
 
 # STEAM (MAIN account)
 # I have not yet implemented getting SteamGuard codes with super-secret thing
 # So both main and test accounts should be without 2FA from SteamGuard
-STEAM_MAIN_LGN = ''
-STEAM_MAIN_PSW = ''
+STEAM_MAIN_LGN = ""
+STEAM_MAIN_PSW = ""
 
 # STEAM (TEST account)
-STEAM_TEST_LGN = ''
-STEAM_TEST_PSW = ''
+STEAM_TEST_LGN = ""
+STEAM_TEST_PSW = ""
+
+# STEAM API KEY
+STEAM_WEB_API_KEY = ""
 
 # DOTA 2
 DOTA_FRIEND_ID = 123_456_789
 
 # GIT PERSONAL TOKEN
-GIT_PERSONAL_TOKEN = ''
+GIT_PERSONAL_TOKEN = ""
 
 # RIOT
-RIOT_API_KEY = ''
+RIOT_API_KEY = ""
 
 # TWITCH # https://twitchtokengenerator.com/ for TOKEN
-TWITCH_TOKEN = ''
+TWITCH_TOKEN = ""
 
 # POSTGRES DATABASE
-POSTGRES_URL = 'postgresql://user:password@host:port/database'
+POSTGRES_URL = "postgresql://user:password@host:port/database"
 
 # WOLFRAM
 WOLFRAM_TOKEN = ""
 
-### NOT USED BCS THEIR CEO ARE R*****S. 
+### NOT USED BCS THEIR CEO ARE R*****S.
 # # REDDIT
 # REDDIT_CLIENT_ID = ''
 # REDDIT_CLIENT_SECRET = ''
@@ -41,16 +45,16 @@ WOLFRAM_TOKEN = ""
 # REDDIT_USER_AGENT = ''
 # REDDIT_USERNAME = ''
 
-# # TWITTER
+### TWITTER ## NOT USED CAUSE WORSE SH**T AS REDDIT
 # TWITTER_BEARER_TOKEN = ''
 
 # ERROR HANDLER WEBHOOK URL
-ERROR_HANDLER_WEBHOOK_URL = 'https://discord.com/api/webhooks/something'
+ERROR_HANDLER_WEBHOOK_URL = "https://discord.com/api/webhooks/something"
 
 # TEST ERROR HANDLER WEBHOOK URL
-TEST_ERROR_HANDLER_WEBHOOK_URL = 'https://discord.com/api/webhooks/something'
+TEST_ERROR_HANDLER_WEBHOOK_URL = "https://discord.com/api/webhooks/something"
 
 # LOGGER SPAM WEEBHOOK URL
-SPAM_LOGS_WEBHOOK_URL = 'https://discord.com/api/webhooks/something'
+SPAM_LOGS_WEBHOOK_URL = "https://discord.com/api/webhooks/something"
 
 # /* cSpell:enable */

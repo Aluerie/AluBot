05865a44d43621b8402f49fa528b02458f219603
Aluerie❤ <aluerie@outlook.com>
2024-02-28 16:40:10 +0300

    Update info.py

--- ext/information/info.py
+++ ext/information/info.py
@@ -92,7 +92,7 @@ class Info(InfoCog, name="Info", emote=const.Emote.PepoG):
         """Show GMT (UTC) time."""
         now_time = discord.utils.utcnow().strftime("%H:%M:%S")
         now_date = discord.utils.utcnow().strftime("%d/%m/%Y")
-        e = discord.Embed(colour=const.Colour.blueviolet, title="GMT(Greenwich Mean Time)")
+        e = discord.Embed(colour=const.Colour.blueviolet, title="GMT (Greenwich Mean Time)")
         e.set_footer(text="GMT is the same as UTC (Universal Time Coordinated)")
         e.add_field(name="Time:", value=now_time).add_field(name="Date:", value=now_date)
         await ctx.reply(embed=e)

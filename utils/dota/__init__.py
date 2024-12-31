f297ccb7a5a2a43668f95141cd101b5733c3caa5
Aluerie❤ <aluerie@outlook.com>
2024-12-31 19:20:05 +0300

    🪥Fix circular import

--- utils/dota/__init__.py
+++ utils/dota/__init__.py
@@ -1,6 +1,4 @@
 from .constants import *
-from .dota2client import *  # VALVE_SWITCH
-from .pulsefire_clients import *
+from .dota2client import *
 from .schemas import *
 from .storage import *
-# from .valvepython import *  # VALVE_SWITCH

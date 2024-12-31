df249bfc584dcec5f217dd65f463015db5d53e18
Aluerie❤ <aluerie@outlook.com>
2024-12-31 19:12:08 +0300

    Update __init__.py

--- utils/dota/__init__.py
+++ utils/dota/__init__.py
@@ -1,6 +1,6 @@
 from .constants import *
+from .dota2client import *  # VALVE_SWITCH
 from .pulsefire_clients import *
 from .schemas import *
-from .steamio import *  # VALVE_SWITCH
 from .storage import *
 # from .valvepython import *  # VALVE_SWITCH

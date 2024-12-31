dd44e4bf74fec694159f653e0c9de52aa0eb1cde
Aluerie❤ <aluerie@outlook.com>
2024-12-31 15:55:13 +0300

    🍭Update config.py

--- config.example.py
+++ config.example.py
@@ -7,9 +7,32 @@ Notes.
 * I use different bot accounts and some different secrets for production and testing versions of the bot.
     This is why there are some differences in variable values for Home PC (Windows) and VPS (Linux)
     which are covered by `if platform.system() == "Linux": condition.
-* It's probably not extremely awful practice rather than always be checking for `if bot.test` in the code.
+* I have type-hints ": str" for the purpose to be safer when streaming VSCode window -
+    this way if we hover over `config.TTG_ACCESS_TOKEN` in other files
+    it will show type str instead of Literal["my actual token"]
 """
+
 import platform
+
+__all__ = (
+    "DISCORD_BOT_TOKEN",
+    "POSTGRES_URL",
+    "STEAM_USERNAME",
+    "STEAM_PASSWORD",
+    "STEAM_WEB_API_KEY",
+    "DOTA_FRIEND_ID",
+    "TTV_DEV_CLIENT_ID",
+    "TTV_DEV_CLIENT_SECRET",
+    "ERROR_WEBHOOK",
+    "SPAM_WEBHOOK",
+    "LOGGER_WEBHOOK",
+    "DOTA_NEWS_WEBHOOK",
+    "ERROR_PING",
+    "STRATZ_BEARER_TOKEN",
+    "WOLFRAM_TOKEN",
+    "GIT_PERSONAL_TOKEN",
+    "RIOT_API_KEY",
+)
 
 # /* cSpell:disable */
 
@@ -19,12 +42,12 @@ import platform
 
 if platform.system() == "Linux":
     # AluBot - my main bot
-    __discord_bot_token = "abcdefghijklmnopqrstuvwxyz_abcdefghijklmnopqrstuvwxyz_abcdefghijklmnopqrstuvwxyz"
+    __discord_bot_token: str = "abcdefghijklmnopqrstuvwxyz_abcdefghijklmnopqrstuvwxyz_abcdefghijklmnopqrstuvwxyz"
 else:
     # YenBot - testing bot
-    __discord_bot_token = "xyz"
+    __discord_bot_token: str = "xyz"
 
-DISCORD_BOT_TOKEN = __discord_bot_token
+DISCORD_BOT_TOKEN: str = __discord_bot_token
 
 ############################################
 #              2. DATABASE                 #
@@ -32,64 +55,58 @@ DISCORD_BOT_TOKEN = __discord_bot_token
 
 if platform.system() == "Linux":
     # VPS Machine
-    __postgres_url = "postgresql://user:password@host:port/database"
+    __postgres_url: str = "postgresql://user:password@host:port/database"
 else:
     # Home Computer
-    __postgres_url = "postgresql://user:password@host:port/database"
+    __postgres_url: str = "postgresql://user:password@host:port/database"
 
-POSTGRES_URL = __postgres_url
+POSTGRES_URL: str = __postgres_url
 
 ############################################
 #               3. STEAM                   #
 ############################################
 
-STEAM_USERNAME = ""
-STEAM_PASSWORD = ""
+STEAM_USERNAME: str = ""
+STEAM_PASSWORD: str = ""
 
 # Steam WEB API Key
-STEAM_WEB_API_KEY = ""
-# Dota 2 my own Friend ID
-DOTA_FRIEND_ID = 123
+STEAM_WEB_API_KEY: str = ""
+# Dota 2 Aluerie's Friend ID
+DOTA_FRIEND_ID: int = 123
 
 ############################################
 #               4. TWITCH                  #
 ############################################
 
-# chat: read
-# chat: edit
-TTG_ACCESS_TOKEN: str = ""
-TTG_REFRESH_TOKEN: str = ""
-TTG_CLIENT_ID: str = ""
-
-# channel:read:redemptions
-TTG_IRENE_ACCESS_TOKEN: str = ""
-TTG_IRENE_REFRESh_TOKEN: str = ""
-TTG_IRENE_CLIENT_ID: str = ""
+TTV_DEV_CLIENT_ID: str = ""
+TTV_DEV_CLIENT_SECRET: str = ""
 
 ############################################
 #         5. WEBHOOK URL CATALOG           #
 ############################################
 
 # Error Handler - send tracebacks and error pings into a proper channel;
-ERROR_HANDLER_WEBHOOK = "https://discord.com/api/webhooks/1234567/ABCDEFG"
+ERROR_WEBHOOK: str = "https://discord.com/api/webhooks/1234567/ABCDEFG"
 # Spam Messages - just miscallenous messages, can be anything;
-SPAM_WEBHOOK = "https://discord.com/api/webhooks/1234567/ABCDEFG"
+SPAM_WEBHOOK: str = "https://discord.com/api/webhooks/1234567/ABCDEFG"
 # Logger - send `log.info`, `log.warning`, `log.error` into #🐬logger;
-LOGGER_WEBHOOK = "https://discord.com/api/webhooks/1234567/ABCDEFG"
+LOGGER_WEBHOOK: str = "https://discord.com/api/webhooks/1234567/ABCDEFG"
 # Dota 2 News Webhook - send Dota 2 news to #🍋dota2_news (or to #🍷test_spam when testing)
-DOTA_NEWS_WEBHOOK = "https://discord.com/api/webhooks/1234567/ABCDEFG"
+DOTA_NEWS_WEBHOOK: str = "https://discord.com/api/webhooks/1234567/ABCDEFG"
+# Ping Aluerie with this
+ERROR_PING: str = "<@&1234>"
 
 ############################################
 #             6. OTHER SERVICES            #
 ############################################
 
 # Stratz
-STRATZ_BEARER_TOKEN = ""
+STRATZ_BEARER_TOKEN: str = ""
 # Wolfram # 2000 /month
-WOLFRAM_TOKEN = ""
+WOLFRAM_TOKEN: str = ""
 # Git Personal Token
-GIT_PERSONAL_TOKEN = ""
+GIT_PERSONAL_TOKEN: str = ""
 # Riot API Key
-RIOT_API_KEY = ""
+RIOT_API_KEY: str = ""
 
 # /* cSpell:enable */

8930d86979fa967ca81cba06280c6006ab876063
Irene Adler❤ <aluerie@outlook.com>
2025-02-21 03:38:08 +0300

    Update steamio_client.py

--- utils/dota/steamio_client.py
+++ utils/dota/steamio_client.py
@@ -38,9 +38,9 @@ class DotaClient(Client):
         self.bot: AluBot = bot
         self.started: bool = False
 
-        # # TODO: EXPERIMENT
-        # # https://discord.com/channels/678629505094647819/1019749658551144458/1341421914714804296
-        # self.http.api_key = config["TOKENS"]["STEAM"]
+        # TODO: EXPERIMENT
+        # https://discord.com/channels/678629505094647819/1019749658551144458/1341421914714804296
+        self.http.api_key = config["TOKENS"]["STEAM"]
 
         # clients
         self.stratz = StratzClient()

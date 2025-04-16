a0a95f7f7e6bd9d03e1bd737bd3b63037eb54ef3
Irene Adler❤ <aluerie@outlook.com>
2025-04-16 19:43:02 +0300

    Update personal.py

--- ext/hideout/personal.py
+++ ext/hideout/personal.py
@@ -14,24 +14,25 @@ if TYPE_CHECKING:
 
 
 class HideoutPersonal(AluCog):
-    @commands.Cog.listener(name="on_message")
-    async def personal_git_copy_paste(self, message: discord.Message) -> None:
-        """Cringe moment, sends."""
-        if message.channel.id == const.Channel.alubot_github:
-            # Send me discord notifications when somebody except me or dependabot
-            # spams my repository with updates
-            embeds = [e.copy() for e in message.embeds]
+    # @commands.Cog.listener(name="on_message")
+    # async def personal_git_copy_paste(self, message: discord.Message) -> None:
+    #     """Cringe moment, sends."""
+    #     if message.channel.id == const.Channel.alubot_github:
+    #         # Send me discord notifications when somebody except me or dependabot
+    #         # spams my repository with updates
+    #         embeds = [e.copy() for e in message.embeds]
 
-            for e in embeds:
-                if e.author and e.author.name not in {self.bot.developer, "dependabot[bot]"}:
-                    await self.hideout.repost.send(embeds=embeds)
-                    break
+    #         for e in embeds:
+    #             if e.author and e.author.name not in {self.bot.developer, "dependabot[bot]"}:
+    #                 await self.hideout.repost.send(embeds=embeds)
+    #                 break
 
-        elif message.channel.id == const.Channel.steamdb_news:
-            # yoink pushed N commits messages from the Steam Tracker
-            if message.embeds and message.embeds[0].title and "pushed" in message.embeds[0].title:
-                embed = message.embeds[0].copy()
-                await self.hideout.repost.send(embed=embed)
+    #     elif message.channel.id == const.Channel.steamdb_news:
+    #         # yoink pushed N commits messages from the Steam Tracker
+    #         if message.embeds and message.embeds[0].title and "pushed" in message.embeds[0].title:
+    #             embed = message.embeds[0].copy()
+    #             await self.hideout.repost.send(embed=embed)
+    pass
 
 
 async def setup(bot: AluBot) -> None:

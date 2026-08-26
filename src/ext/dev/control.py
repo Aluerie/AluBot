4e42f471535b5f1c835237fb1c9c470eb96e3698
Aluerie <aluerie@gmail.com>
2026-08-26 20:44:54 +0300

    Update control.py

--- src/ext/dev/control.py
+++ src/ext/dev/control.py
@@ -42,7 +42,7 @@ class Control(BaseDevCog):
         self.database_backup.stop()
         await super().cog_unload()
 
-    @aluloop(time=datetime.time(hour=17, minute=35, tzinfo=datetime.UTC))
+    @aluloop(time=datetime.time(hour=17, minute=50, tzinfo=datetime.UTC))
     async def database_backup(self) -> None:
         """Daily task to backup the bot's database.
 
@@ -51,7 +51,7 @@ class Control(BaseDevCog):
         """
         if self.bot.is_vps:
             await asyncio.create_subprocess_shell("pg_dump alubot > .temp/alubot.sql")
-            await self.hideout.database.send(file=discord.File(".temp/alubot.sql"))
+            await self.hideout.database.send(file=discord.File(fp=".temp/alubot.sql", filename="alubot.sql"))
 
     system_group = app_commands.Group(
         name="system-dev",  # TODO: maybe rename this to alubot-dev so it matches irebot-dev :thinking:
